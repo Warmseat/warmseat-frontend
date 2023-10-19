d136688b36974d6cf16ceded898eee2e02fdea3a
Quasark <q@quasark.xyz>
2023-10-19 11:52:42 -0700

    auth almost ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import Home from "../components/Homepage";
 import About from "../components/About";
 import Header from '../components/Header';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+
+function App({ signOut, user }) {
   return (
     <Router>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
